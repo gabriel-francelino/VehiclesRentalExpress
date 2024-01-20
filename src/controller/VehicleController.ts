9db629edd967be648c02190910ef05d0c5c36bdd
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-01-19 22:02:16 -0300

    começando funcionalidades do veiculo

---
+++ src/controller/VehicleController.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+class VehicleController {
+    create(req: Request, res: Response, next: NextFunction) {
+
+    }
+
+    get(req: Request, res: Response, next: NextFunction) {
+
+    }
+
+    getById(req: Request, res: Response, next: NextFunction) {
+
+    }
+
+    update(req: Request, res: Response, next: NextFunction) {
+
+    }
+
+    delete(req: Request, res: Response, next: NextFunction) {
+
+    }
+}
+
+const vehicleController = new VehicleController();
+
+export { vehicleController };
