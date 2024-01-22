c5fb3f204532c693874c1df74c3c8bbe4d129398
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-01-21 21:50:04 -0300

    customer repository criando

--- src/repositories/CustomerRepository.ts
+++ src/repositories/CustomerRepository.ts
@@ -1,13 +1,59 @@
-import { Customer, ECategoryType } from "../models/customer";
+import { Customer } from "../models/Customer";
+
+
 
 class CustomerRepository {
     private customers: Customer[] = [
-        new Customer("12345678910", "João", new Date("1990-01-01"), "A" as ECategoryType),
-        new Customer("12345678911", "Pedro", new Date("2002-12-20"), ECategoryType.B),
-        new Customer("12345678912", "Daniel", new Date("2003-05-03"), ECategoryType.B),
-        new Customer("12345678913", "Carlos", new Date("1998-01-15"), ECategoryType.B),
-        new Customer("12345678914", "Gabriel", new Date("1997-11-22"), ECategoryType.A),
+        new Customer("15948726301", "João da Silva", new Date("1990-01-01"), "AB"),
+        new Customer("75395185202", "Maria da Silva", new Date("2001-01-01"), "B"),
+        new Customer("45678912303", "José da Silva", new Date("1998-01-01"), "B"),
+        new Customer("12345678904", "Ana da Silva", new Date("1998-01-01"), "AB"),
+        new Customer("98765432105", "Paulo da Silva", new Date("2000-01-01"), "A"),
+        new Customer("74185296306", "Marta da Silva", new Date("1987-01-01"), "B"),
+        new Customer("85296374107", "Carlos da Silva", new Date("1990-01-01"), "AB")
     ];
+
+    public create(customer: Customer): Customer {
+        this.customers.push(customer);
+
+        return customer;
+    }
+
+    public getAll(): Customer[] {
+        return this.customers;
+    }
+
+    public getById(id: string): Customer | undefined {
+        return this.customers.find(customer => customer.id === id);
+    }
+
+    public getByCpf(cpf: string): Customer | undefined {
+        return this.customers.find(customer => customer.cpf === cpf);
+    }
+
+    public update(updatedCustomer: Customer): Customer | undefined {
+        const index = this.customers.findIndex(customer => customer.id === updatedCustomer.id);
+
+        if (index === -1) {
+            return undefined;
+        }
+
+        this.customers[index] = updatedCustomer;
+        return this.customers[index];
+    }
+
+    public delete(id: string): Customer | undefined {
+        const index = this.customers.findIndex(customer => customer.id === id);
+
+        if (index === -1) {
+            return undefined;
+        }
+
+        const deletedCustomer = this.customers[index];
+        this.customers.splice(index, 1);
+
+        return deletedCustomer;
+    }
 }
 
 const customerRepository = new CustomerRepository();
