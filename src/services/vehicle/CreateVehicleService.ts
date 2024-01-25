1e0a7eabc92e444da96d334045ac4e60dea96fc0
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-01-24 22:04:49 -0300

    refatorando

--- src/services/vehicle/CreateVehicleService.ts
+++ src/services/vehicle/CreateVehicleService.ts
@@ -6,11 +6,12 @@ import { vehicleRepository } from "../../repositories/VehicleRepository";
 class CreateVehicleService {
     execute(vehicle: Vehicle): Vehicle{
         const vehicles: Vehicle[] = vehicleRepository.getAll();
+        
         vehicles.forEach(v => {
             if (v.plate === vehicle.plate) {
                 throw new AppError("Vehicle with this plate already exists!", StatusCodes.CONFLICT);
             }
-        })
+        });
 
         const newVehicle: Vehicle = vehicleRepository.create(vehicle);
 
