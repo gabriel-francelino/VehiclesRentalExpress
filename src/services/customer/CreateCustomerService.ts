c2294938aa63cd83129586f24eb8466cd0461df4
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-01-24 22:04:39 -0300

    validação para não criar customer com cpf já adicionado

--- src/services/customer/CreateCustomerService.ts
+++ src/services/customer/CreateCustomerService.ts
@@ -1,8 +1,18 @@
+import { StatusCodes } from "http-status-codes";
+import { AppError } from "../../error/AppError";
 import { Customer } from "../../models/Customer";
 import { customerRepository } from "../../repositories/CustomerRepository";
 
 class CreateCustomerService {
     execute(customer: Customer): Customer {
+        const customers: Customer[] = customerRepository.getAll();
+
+        customers.forEach(c => {
+            if (c.cpf === customer.cpf) {
+                throw new AppError("Customer with this CPF already exists!", StatusCodes.CONFLICT);
+            }
+        });
+
         const createdCustomer = customerRepository.create(customer);
 
         return createdCustomer;
