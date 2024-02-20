9b4fd6e9f9b03e48bdd7cf0c6e913ec6719475f5
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-02-20 20:12:44 -0300

    feat: added env validation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 
 import { routes } from './routes';
+import { env } from './env';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+const port = env.PORT
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+});
