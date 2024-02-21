2327dd3849bfe56ad612bb3531f5010f51d1bf33
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-02-21 14:08:26 -0300

    refact: rental services

--- src/app/services/rental/GetAllActiveRentalService.ts
+++ src/app/services/rental/GetAllActiveRentalService.ts
@@ -1,12 +1,21 @@
-import { Rental } from '../../models/Rental'
-import { rentalRepository } from '../../repositories/inMemory/InMemoryRentalRepository'
+import { RentalRepository } from '@/infra/database/repositories/IRentalRepository'
+import { Rental, RentalProps } from '../../models/Rental'
 
-class GetAllActiveRentalService {
-  execute(): Rental[] {
-    return rentalRepository.getAllActive()
-  }
+interface GetAllActiveRentalServiceResponse {
+  activeRentals: Rental[]
 }
 
-const getAllActiveRentalService = new GetAllActiveRentalService()
+export class GetAllActiveRentalService {
+  constructor(private rentalRepository: RentalRepository) {}
 
-export { getAllActiveRentalService }
+  // async execute(): Promise<GetAllActiveRentalServiceResponse> {
+  //   const rentalsData = (await this.rentalRepository.findAll()).filter((rental) => !rental.hasRent)
+
+  //   const activeRentals: Rental[] = rentalsData.map(
+  //     (rentalData) => new Rental(rentalData as RentalProps),
+  //   )
+  //   return {
+  //     activeRentals,
+  //   }
+  // }
+}
