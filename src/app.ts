4c6eee2ce84b0a981b6677d475c63d1e31078f28
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-02-22 04:28:53 -0300

    feat: added config nodemon and basic config swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './swagger.json'
 
 import { routes } from './app/routes'
 import { env } from './env'
@@ -6,9 +8,10 @@ import { env } from './env'
 const app = express()
 
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(routes)
 const port = env.PORT
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port} 🏍️`)
 })
