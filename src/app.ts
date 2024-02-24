f801411e0eb0f0191966b4148abf4b4b538940bc
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-02-24 19:19:45 -0300

    feat: added helmet, cors and requisition logs

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,22 @@ import swaggerDocs from './swagger.json'
 
 import { routes } from './app/routes'
 import { env } from './env'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
 
 const app = express()
+const corsOptions = {
+  origin: 'http://localhost:5173',
+}
+app.use(cors(corsOptions))
+
+app.use(
+  morgan(
+    '📅 [:date[iso]] - [:method] - :url - status-code: [:status] - duration: :response-time ms',
+  ),
+)
+app.use(helmet())
 
 app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
